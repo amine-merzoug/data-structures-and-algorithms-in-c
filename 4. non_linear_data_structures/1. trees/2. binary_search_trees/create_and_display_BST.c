d31e7446ed207c88f8a8a6945cf3ffd9688dfdc6
amine-merzoug <amine.merzoug@gmail.com>
2023-11-22 18:35:54 +0100

    Add create_and_display_BST.c example

---
+++ 4. non_linear_data_structures/1. trees/2. binary_search_trees/create_and_display_BST.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tree_node* ptr;
+struct tree_node{
+    int data;
+    ptr left;
+    ptr right;
+};
+
+ptr new_node(int);
+void display_tree(ptr);
+
+/* The objective in this first example is to create and display 
+the following binary search tree (BST):
+             20
+            /  \
+          5    30
+         /  \    
+        4    7                                   
+*/
+
+int main(){
+    // Create BST
+    ptr root = new_node(20);
+    root->left = new_node(5);
+    root->right = new_node(30);
+    root->left->right = new_node(7);
+    root->left->left = new_node(4);
+
+    printf("\nBST: ");
+    display_tree(root);
+    printf("\n\n");
+}
+
+// creates a new BST node and returns its address
+ptr new_node(int data){
+    ptr p = malloc(sizeof(struct tree_node));
+    p->data  = data;
+    p->left  = NULL;
+    p->right = NULL;
+    return p;
+}
+
+// prints the given BST using in-order traversal
+void display_tree(ptr root){
+    if (root != NULL){
+        display_tree(root->left);
+        printf("%i ", root->data);
+        display_tree(root->right);
+    }
+}
